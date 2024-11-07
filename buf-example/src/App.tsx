45a6bcc4125aead4042f137e5cf725287d206028
araj-dev <ja.fileonly@gmail.com>
2024-11-07 13:46:44 +0900

    use json data from api

--- buf-example/src/App.tsx
+++ buf-example/src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useState} from "react";
 
 function App() {
-    return <>Hello world</>;
+    const [user, setUser] = useState(false)
+    useEffect(() => {
+        fetch("http://localhost:8080/v1.UserService/GetUser", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({"hi": "hi"})
+        }).then(res => res.json().then(data => setUser(data)))
+    })
+
+    return <>
+        id: {user.id} <br/>
+        username: {user.username}
+    </>;
 }
 
 export default App
